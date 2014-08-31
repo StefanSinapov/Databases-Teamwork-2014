1ae0ced394d78eaaf27728b125a442d768cc8c55
izabelk <izi_girl@abv.bg>
2014-08-31 22:34:32 +0300

    Refacrored XmlLoader.cs class

--- CarsFactory/CarsFactory.Loaders/XmlLoader.cs
+++ CarsFactory/CarsFactory.Loaders/XmlLoader.cs
@@ -9,82 +9,101 @@
 
     public class XmlLoader
     {
-        
+        private const string fileName = @"..\..\..\Xml-Data-Load\Manufacturers-Expenses.xml";
+        private const string manufacturerTagName = "manufacturer";
+        private const string expensesTagName = "expenses";
+        private const string nameAttribute = "name";
+        private const string monthAttribute = "month";
+
         public static void LoadXmlFile(CarsFactoryContext context)
         {
-            string fileName = @"..\..\..\Xml-Data-Load\Manufacturers-Expenses.xml";
-
             using (XmlReader reader = XmlReader.Create(fileName))
             {
                 while (reader.Read())
                 {
                     if ((reader.NodeType == XmlNodeType.Element) &&
-                        (reader.Name == "manufacturer"))
+                        (reader.Name == manufacturerTagName))
                     {
-                        var manafacturerName = reader.GetAttribute("name");
-                        var manufacturerExists = context.Manufacturers.Any(m => m.Name == manafacturerName);
-
-                        if (!manufacturerExists)
-                        {
-                            var manafacturer = new Manufacturer
-                            {
-                                Name = manafacturerName
-                            };
-                            context.Manufacturers.Add(manafacturer);
-                            context.SaveChanges();
-                        }
+                        var manufacturerName = reader.GetAttribute(nameAttribute);
+                        AddManufacturer(context, manufacturerName);
 
                         while (reader.Read())
                         {
                             if ((reader.NodeType == XmlNodeType.Element) &&
-                                (reader.Name == "expenses"))
+                                (reader.Name == expensesTagName))
                             {
-                                var monthName = reader.GetAttribute("month");
-                                var expenseeValue = decimal.Parse(reader.ReadElementString());
+                                var monthName = reader.GetAttribute(monthAttribute);
+                                AddMonth(context, monthName);
 
-                                var monthExists = context.Months.Any(m => m.Name == monthName);
-                                if (!monthExists)
-                                {
-                                    var month = new Month
-                                    {
-                                        Name = monthName
-                                    };
+                                var expenseValue = decimal.Parse(reader.ReadElementString());
 
-                                    context.Months.Add(month);
-                                    context.SaveChanges();
-                                }
-
-                                var manafacturer = context.Manufacturers.First(m => m.Name == manafacturerName);
+                                var manufacturer = context.Manufacturers.First(m => m.Name == manufacturerName);
                                 var selectedMonth = context.Months.First(m => m.Name == monthName);
 
-                                var expense = new Expense
-                                {
-                                    MonthId = selectedMonth.MonthId,
-                                    ManafacturerId = manafacturer.Id,
-                                    Value = expenseeValue
-                                };
-
-                                var expenseExists = context.Expenses.Any(e => e.ManafacturerId == manafacturer.Id);
-
-                                if (!expenseExists)
-                                {
-                                    context.Expenses.Add(expense);
-                                    context.SaveChanges();
-                                }
-
-                                manafacturer.Expenses.Add(expense);
-                                context.SaveChanges();
-
+                                AddExpense(context, manufacturer, selectedMonth, expenseValue);
                             }
 
                             else if ((reader.NodeType == XmlNodeType.EndElement) &&
-                           (reader.Name == "manufacturer"))
+                                    (reader.Name == manufacturerTagName))
                             {
                                 break;
                             }
                         }
                     }
                 }
+            }
+        }
+
+        private static void AddManufacturer(CarsFactoryContext context, string manufacturerName)
+        {
+            var manufacturerExists = context.Manufacturers.Any(m => m.Name == manufacturerName);
+
+            if (!manufacturerExists)
+            {
+                var manafacturer = new Manufacturer
+                {
+                    Name = manufacturerName
+                };
+
+                context.Manufacturers.Add(manafacturer);
+                context.SaveChanges();
+            }
+        }
+
+        private static void AddMonth(CarsFactoryContext context, string monthName)
+        {
+            var monthExists = context.Months.Any(m => m.Name == monthName);
+
+            if (!monthExists)
+            {
+                var month = new Month
+                {
+                    Name = monthName
+                };
+
+                context.Months.Add(month);
+                context.SaveChanges();
+            }
+        }
+
+        private static void AddExpense(CarsFactoryContext context, Manufacturer manufacturer,
+                                       Month selectedMonth, decimal expenseValue)
+        {
+            var expenseExists = context.Expenses.Any(e => e.ManafacturerId == manufacturer.Id &&
+                                                                         e.MonthId == selectedMonth.MonthId &&
+                                                                         e.Value == expenseValue);
+
+            if (!expenseExists)
+            {
+                var expense = new Expense
+                {
+                    MonthId = selectedMonth.MonthId,
+                    ManafacturerId = manufacturer.Id,
+                    Value = expenseValue
+                };
+
+                context.Expenses.Add(expense);
+                context.SaveChanges();
             }
         }
     }
